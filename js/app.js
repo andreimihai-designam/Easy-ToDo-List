3a329f14373c383e39bd591f6fd08743877c00c7
Andrei Mihai <andrei.designam@gmail.com>
2022-01-22 19:56:24 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -30,19 +30,6 @@ function addItem(){
     })
 
     divChild.appendChild(checkIcon);
-    
-    function deleteCheck (e) {
-        const item = e.target;
-        //DELETE TODO
-        if(item.classList[0] === 'trash-btn') {
-            const todo = item.parentElement; //to remove the todo item parent
-            //ANIMATION
-            todo.classList.add('fall');
-            todo.addEventListener('transitionend', function(){
-                todo.remove();
-            });
-        }
-    }
 
     trashIcon.className = 'fas fa-trash';
     trashIcon.style.color = 'lightgrey';
