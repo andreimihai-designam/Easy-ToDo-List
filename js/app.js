165a95b638667490f4e3772656268bb09b10d727
Andrei Mihai <andrei.designam@gmail.com>
2022-01-22 17:27:04 +0200

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,45 @@
+// gain access to container (.to-do-items)
+// gain access to input (#input)
+// gain access to the delete task icon
+// I. Create the varialbes that are needed:
+const toDoItems = document.getElementsByClassName('to-do-items')[0];
+const input = document.getElementById('input');
+const trashIcon = document.getElementById('trash');
+
+// II. Create a event listener on the input box, so the user can click on it:
+
+input.addEventListener('keydown', function(event){
+    if(event.key === 'Enter')
+        addItem();
+})
+
+// III. Create the function above:
+function addItem(){
+    var divParent = document.createElement('div');
+    var divChild = document.createElement('div');
+    var checkIcon = document.createElement('i');
+    var trashIcon = document.createElement('i');
+
+    divParent.className = 'item';
+    divParent.innerHTML = '<div>'+input.value+'</div>';
+
+    checkIcon.className = 'fas fa-check-square';
+    checkIcon.style.color = 'lightgrey';
+    checkIcon.addEventListener('click', function(){
+        checkIcon.style.color = 'limegreen';
+    })
+
+    divChild.appendChild(checkIcon);
+
+    trashIcon.className = 'fas fa-trash';
+    trashIcon.style.color = 'lightgrey';
+    trashIcon.addEventListener('click', function(){
+        divParent.remove();
+    })
+
+    divChild.appendChild(trashIcon);
+    divParent.appendChild(divChild);
+    toDoItems.appendChild(divParent);
+
+    input.value = '';
+}
